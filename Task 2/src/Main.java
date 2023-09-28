50526dd865ba848f2c0f5576c9e256502c46217d
123456 <napnest@gmail.com>
2023-09-28 08:59:38 +0300

    Task 2 complete

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+
+        a=inputNumber();
+        checkParity();
+    }
+    static int a;
+    static int inputNumber(){
+        System.out.println("Введите целое число: ");
+        return new Scanner(System.in).nextInt();
+    }
+    static void checkParity(){
+        System.out.print("Введенное число четное: ");
+        System.out.println(a%2==0);
     }
 }
